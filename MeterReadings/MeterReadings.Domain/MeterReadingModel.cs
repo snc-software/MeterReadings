e7f0c3383f2ebf87cac44b9025a1c5d80e1aa6ac
scott.crowther <scottcrowther@hotmail.com>
2021-08-05 17:50:48 +0100

    Ignore Id on CSV

--- MeterReadings/MeterReadings.Domain/MeterReadingModel.cs
+++ MeterReadings/MeterReadings.Domain/MeterReadingModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using CsvHelper.Configuration.Attributes;
 
 namespace MeterReadings.Domain
 {
     public class MeterReadingModel
     {
+        [Ignore]
         public Guid Id { get; set; }
         public int AccountId { get; set; }
         public DateTime MeterReadingDateTime { get; set; }
